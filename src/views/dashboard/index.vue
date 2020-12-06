2d104591c5229aeb1aeba507ba04815052ab8dbc
WangTao <793208187@qq.com>
2020-12-06 20:54:08 +0800

    1.主页改了雏形出来

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,10 +1,33 @@
 <template>
+  <!--  <div class="dashboard-container">-->
+  <!--    <div class="dashboard-text">name: {{ name }}</div>-->
+  <!--    <div class="dashboard-text">birthday: {{ birthday }}</div>-->
+  <!--    <div class="dashboard-text">telephone: {{ telephone }}</div>-->
+  <!--    <div class="dashboard-text">sex: {{ sex }}</div>-->
+  <!--    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>-->
+  <!--  </div>-->
+
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
-    <div class="dashboard-text">birthday: {{ birthday }}</div>
-    <div class="dashboard-text">telephone: {{ telephone }}</div>
-    <div class="dashboard-text">sex: {{ sex }}</div>
-    <div class="dashboard-text">roles: <span v-for="role in roles" :key="role">{{ role }}</span></div>
+    <el-row :gutter="20">
+      <el-col :span="6" :xs="24">
+        {{ name }}
+      </el-col>
+      <el-col :span="18" :xs="24">
+        <el-card>
+          <el-tabs v-model="activeTab">
+            <el-tab-pane label="Activity" name="activity">
+              {{ telephone }}
+            </el-tab-pane>
+            <el-tab-pane label="Timeline" name="timeline">
+              {{ sex }}
+            </el-tab-pane>
+            <el-tab-pane label="Account" name="account">
+              {{ birthday }}
+            </el-tab-pane>
+          </el-tabs>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -13,6 +36,11 @@ import { mapGetters } from 'vuex'
 
 export default {
   name: 'Dashboard',
+  data() {
+    return {
+      activeTab: 'activity'
+    }
+  },
   computed: {
     ...mapGetters([
       'name',
